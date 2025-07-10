1ffd617d0f45674ec52ebeb9297171e2d11c08b2
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-09 20:44:55 -0500

    Cambio de nombre de rutas

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -6,8 +6,6 @@ class ChatProvider extends ChangeNotifier {
   final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
   final ScrollController chatScrollController = ScrollController();
   List<Message> messageList = [
-    Message(text: "Hola amor!", fromWho: FromWho.me),
-    Message(text: "Ya regresaste del trabajo?", fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
